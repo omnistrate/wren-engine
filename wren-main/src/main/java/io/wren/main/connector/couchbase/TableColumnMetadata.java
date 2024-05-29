0fd7fcde00477d072487aa845b094fea1652eaf2
Alok Nikhil <alok@purplpkg.com>
2024-05-28 17:01:35 -0700

    Correct JSON response fields for Couchbase Table Metadata

--- wren-main/src/main/java/io/wren/main/connector/couchbase/TableColumnMetadata.java
+++ wren-main/src/main/java/io/wren/main/connector/couchbase/TableColumnMetadata.java
@@ -1,10 +1,19 @@
 package io.wren.main.connector.couchbase;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * 'SELECT \
  * table_catalog, table_schema, table_name, column_name, ordinal_position, is_nullable, data_type\
  * FROM INFORMATION_SCHEMA.COLUMNS;';
  */
-public record TableColumnMetadata(String tableCatalog, String tableSchema, String tableName, String columnName,
-                                  int ordinalPosition, boolean isNullable, String dataType) {
+public record TableColumnMetadata(
+        @JsonProperty("table_catalog") String tableCatalog,
+        @JsonProperty("table_schema") String tableSchema,
+        @JsonProperty("table_name") String tableName,
+        @JsonProperty("column_name") String columnName,
+        @JsonProperty("ordinal_position") int ordinalPosition,
+        @JsonProperty("is_nullable") boolean isNullable,
+        @JsonProperty("data_type") String dataType
+) {
 }
